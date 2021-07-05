0478af5d7c4eb20e8ea9f61d50c906c15ba7d671
Valerii Honcharov <goncharov.valery.work@gmail.com>
2021-07-05 20:00:02 +0300

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const KILOBYTE = 1024;
+
+const config = {
+    entry: {
+        bundle: './src/index.tsx',
+    },
+    output: {
+        path: path.resolve(__dirname, 'static/build'),
+        filename: '[name].[chunkhash].js',
+        assetModuleFilename: 'images/[hash][ext][query]',
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+            },
+            {
+                use: 'ts-loader',
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+            },
+            {
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                ],
+                test: /\.css$/,
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 40 * KILOBYTE,
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: false,
+        compress: true,
+        port: 3000,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'style.css' }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './static/index.html'),
+            filename: 'index.html',
+            title: 'Interview task',
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+    ],
+};
+
+module.exports = config;
